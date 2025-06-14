0e53c6977bf4b73f9e747d07fe3570e1b970f49b
Alex Patey <alex.patey@btinternet.com>
2025-06-14 20:28:04 +0100

    add kv to retriever api and add test kv endpoint

--- RetrieverApi/src/Essays.Retriever.Api/Program.cs
+++ RetrieverApi/src/Essays.Retriever.Api/Program.cs
@@ -1,12 +1,17 @@
 using Essays.Retriever.Api.Endpoints.Internal;
+using Essays.Retriever.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.ConfigureAzureKeyVault();
 builder.Services.AddEndpoints<Program>(builder.Configuration);
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.MapGet("test-key-vault", (IConfiguration configuration) => configuration["TestKey"]);
+
 app.UseEndpoints<Program>();
 
 app.Run();
