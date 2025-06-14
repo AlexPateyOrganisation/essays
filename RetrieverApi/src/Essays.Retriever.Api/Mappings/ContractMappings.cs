504b80f5f073ee0926d1b2713e8bdf57d5fca71f
Alex Patey <alex.patey@btinternet.com>
2025-06-14 17:07:08 +0100

    run dotnet format

--- RetrieverApi/src/Essays.Retriever.Api/Mappings/ContractMappings.cs
+++ RetrieverApi/src/Essays.Retriever.Api/Mappings/ContractMappings.cs
@@ -6,6 +6,6 @@ namespace Essays.Retriever.Api.Mappings;
 
 public static class ContractMappings
 {
-    public static EssayResponse MapToEssayResponse(this Essay essay) => 
+    public static EssayResponse MapToEssayResponse(this Essay essay) =>
         new(essay.Id, essay.Title, essay.CompressedBody.DecompressWithGzip(), essay.Author, essay.CreatedAt);
 }
