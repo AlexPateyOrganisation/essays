e9d03d2dc1cd1633c94a2182731805efe2306238
AlexPatey <57450484+AlexPatey@users.noreply.github.com>
2025-07-23 20:54:50 +0100

    add telemetry to retriever api (#28)
    * add telemetry to retriever api
    
    * format code

--- RetrieverApi/src/Essays.Retriever.Api/Extensions/ConfigurationExtensions.cs
+++ RetrieverApi/src/Essays.Retriever.Api/Extensions/ConfigurationExtensions.cs
@@ -1,4 +1,10 @@
+using System.Reflection;
 using Azure.Identity;
+using Azure.Monitor.OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace Essays.Retriever.Api.Extensions;
 
@@ -13,5 +19,74 @@ public static class ConfigurationExtensions
                 new Uri($"https://{keyVaultName}.vault.azure.net/"),
                 new DefaultAzureCredential());
         }
+    }
+
+    public static void ConfigureOpenTelemetry(this WebApplicationBuilder builder)
+    {
+        const string serviceName = "essays-retriever-api";
+        const string serviceNamespace = "Essays.Retriever.Api";
+
+        var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version!.ToString();
+        var applicationInsightsConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+
+        builder.Services.AddOpenTelemetry()
+            .ConfigureResource(resource => resource
+                .AddService(serviceName, serviceNamespace, serviceVersion: assemblyVersion)
+                .AddAttributes([new KeyValuePair<string, object>("environment", builder.Environment.EnvironmentName)]))
+            .WithTracing(tracing =>
+            {
+                tracing.AddAspNetCoreInstrumentation();
+                tracing.AddEntityFrameworkCoreInstrumentation(options =>
+                        options.SetDbStatementForText = builder.Environment.IsDevelopment());
+                tracing.AddRedisInstrumentation();
+                tracing.AddConsoleExporter();
+                if (applicationInsightsConnectionString is null)
+                {
+                    var otlpEndpoint = builder.Configuration["OpenTelemetry:OTLP_Endpoint"] ??
+                                       throw new Exception("Could not find url for OpenTelemetry Collector.");
+
+                    tracing.AddOtlpExporter(options => options.Endpoint = new Uri(otlpEndpoint));
+                }
+                else
+                {
+                    tracing.AddAzureMonitorTraceExporter(options
+                        => options.ConnectionString = applicationInsightsConnectionString);
+                }
+            })
+            .WithMetrics(metrics =>
+            {
+                metrics.AddAspNetCoreInstrumentation();
+                metrics.AddMeter("Microsoft.AspNetCore.Hosting");
+                metrics.AddMeter("Microsoft.AspNetCore.Server.Kestrel");
+                metrics.AddConsoleExporter();
+                if (applicationInsightsConnectionString is null)
+                {
+                    var otlpEndpoint = builder.Configuration["OpenTelemetry:OTLP_Endpoint"] ??
+                                       throw new Exception("Could not find url for OpenTelemetry Collector.");
+
+                    metrics.AddOtlpExporter(options => options.Endpoint = new Uri(otlpEndpoint));
+                }
+                else
+                {
+                    metrics.AddAzureMonitorMetricExporter(options =>
+                        options.ConnectionString = applicationInsightsConnectionString);
+                }
+            })
+            .WithLogging(logging =>
+            {
+                logging.AddConsoleExporter();
+                if (applicationInsightsConnectionString is null)
+                {
+                    var otlpEndpoint = builder.Configuration["OpenTelemetry:OTLP_Endpoint"] ??
+                                       throw new Exception("Could not find url for OpenTelemetry Collector.");
+
+                    logging.AddOtlpExporter(options => options.Endpoint = new Uri(otlpEndpoint));
+                }
+                else
+                {
+                    logging.AddAzureMonitorLogExporter(options =>
+                        options.ConnectionString = applicationInsightsConnectionString);
+                }
+            });
     }
 }
